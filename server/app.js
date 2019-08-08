6da900c5a7bc0196bedad6dfda31fa00d4854a06
Stefano and jmourell <steppashr+j.mourell@gmail.com>
2019-08-08 21:41:02 +0900

    Implement basic graphQL schema for locations

--- server/app.js
+++ server/app.js
@@ -10,17 +10,65 @@ require("dotenv").config();
 const app = express();
 
 const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
 
-const root = { hello: () => "Hello world!" };
+  type Location {
+    id: Int,
+    latitude: Float,
+    longitude: Float,
+    name: String,
+    address: String,
+    state: String,
+    city: String,
+    highway:String,
+  }
+
+  type Query {
+    locations: [Location]
+    location (name: String, id: Int): Location
+  }
+
+  `);
+const Location = function(data) {
+  this.id = data.id;
+  this.longitude = data.longitude;
+  this.latitude = data.latitude;
+  this.name = data.name;
+  this.address = data.address1;
+  this.state = data.state;
+  this.city = data.city;
+  this.highway = data.highway;
+};
+
+const root = {
+  Locations: async () => {
+    try {
+      const locations = await db.select().table("locations");
+      console.log("heyyyy", locations);
+      return locations.map((location) => new Location(location));
+    } catch (err) {
+      console.error("Error loading locations!", err);
+    }
+  },
+  Location: async (req) => {
+    try {
+      const name = req.Name;
+
+      const locations = await db
+        .select()
+        .where("name", name)
+        .table("locations");
+      console.log(locations);
+      return locations.map((location) => new Location(location))[0];
+    } catch (err) {
+      console.error("Error loading location!", err);
+    }
+  },
+};
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
+    schema,
     rootValue: root,
     graphiql: true,
   })
@@ -54,3 +102,83 @@ app.get("*", (req, res) => {
 });
 
 module.exports = app;
+
+/*
+  type AcceptedPaymentTypes {
+    SiteManagementItem: [SiteManagementItem]
+  },
+
+  type SiteManagementItem {
+    Title: String,
+    Section: String
+  },
+
+  type AdditionalAmenities {
+    SiteManagementItem: [SiteManagementItem]
+  }
+
+  type Address {
+    Address1: String,
+    Address2: String,
+    AddressId: Number,
+    City: String,
+    Name: String,
+    State: String,
+    Zip: String
+  }
+
+  type ContactMethod {
+    Type: Type,
+    Id: Number,
+    # Phone Number in String 
+    Data: String 
+  },
+
+  type Type {
+    Id: Number,
+    Name: String
+  },
+
+  type CustomFields {
+    CustomField: [CustomField]
+  },
+  type FilteredCustomFields {
+    CustomField: [CustomField]
+  },
+
+  type CustomField {
+    Id: Number,
+    Label: String,
+    Name: String,
+    Section: String
+  },
+
+  type Site {
+    FuelPrices: [Fuelprice],
+    Latitude: Float,
+    Longitude: Float,
+    Highway: String,
+    DescriptiveAddress: String,
+    ExitNumber: String,
+    SiteName: String,
+    SiteId: Number
+  }
+
+  type FuelPrice {
+    SiteId: Number,
+    FieldType: String,
+    CashPrice: Number,
+    DisplayName: String
+  },
+
+  type Station {
+
+  }
+
+  type Query {
+    AcceptedPaymentTypes: AcceptedPaymentTypes,
+    AdditionalAmenities: AdditionalAmenities,
+
+  }
+
+*/

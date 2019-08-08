3215f32909bc5b1b0932157597edd798fe3550f8
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-08 15:01:51 +0900

    Added SearchPage on Top of Goh's

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Map from "./containers/Map";
 import Result from "./components/Result";
+import { SearchPage } from "./components/SearchPage";
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: "100%" }}>
         <Map id="map" />
+        <SearchPage />
         <Result />
       </div>
     );

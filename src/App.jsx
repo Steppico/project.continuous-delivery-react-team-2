4690596e616dd915515d13f575dd43643c51b5f3
Steppico <steppashr@gmail.com>
2019-08-10 18:29:14 +0900

    Move NavBar with Title above MAP

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="titleBanner">Flying Truck Stops</div>
         <Map id="map" />
         <SearchPage />
         <br />
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -5,7 +5,6 @@ import { SearchCriteria } from "./SearchCriteria";
 export const SearchPage = () => {
   return (
     <div>
-      <div className="titleBanner">Flying Truck Stops</div>
       <div className="SearchResultPhoto">Locations Map here</div>
       <SearchCriteria />
     </div>

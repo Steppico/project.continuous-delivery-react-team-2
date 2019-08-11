75ce212e5d2859cf7ed4c073b5862fa10013a59c
ryohei and Julian <watanaber17+j.mourell@gmail.com>
2019-08-11 17:50:34 +0900

    Add graphQL fuel schema and query to graphQL

--- server/app.js
+++ server/app.js
@@ -5,22 +5,118 @@ const path = require("path");
 const db = require("./knex.js");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
-require("dotenv").config();
 
 const app = express();
 
 const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
 
-const root = { hello: () => "Hello world!" };
+  type Location {
+    id: Int,
+    latitude: Float,
+    longitude: Float,
+    name: String,
+    address: String,
+    state: String,
+    city: String,
+    highway:String,
+  }
+
+  type State {
+    name: String,
+    cities: [String],
+    highways: [String]
+  }
+
+
+  type Fuel {
+    id: Int,
+    locationId: Int,
+    name: String,
+    price: Float,
+  }
+
+  type Query {
+    locations(name: String, state:String, city:String, highway:String): [Location],
+    states: [State],
+    fuels: [Fuel]
+  }
+
+  `);
+const Location = function(data) {
+  this.id = data.id;
+  this.longitude = data.longitude;
+  this.latitude = data.latitude;
+  this.name = data.name;
+  this.address = data.address1;
+  this.state = data.state;
+  this.city = data.city;
+  this.highway = data.highway;
+};
+
+const Fuel = function(data) {
+  this.id = data.id;
+  this.locationId = data.location_id;
+  this.name = data.name;
+  this.price = data.price.toFixed(2);
+};
+
+const root = {
+  locations: async (req) => {
+    try {
+      let locations;
+      if (Object.keys(req).length === 0) {
+        locations = await db.select().table("locations");
+      } else {
+        locations = await db
+          .select()
+          .where(req)
+          .table("locations");
+      }
+      return locations.map((location) => new Location(location));
+    } catch (err) {
+      console.error("Error loading locations!", err);
+    }
+  },
+  fuels: async (req) => {
+    try {
+      const fuels = await db.select().table("fuel");
+      return fuels.map((fuel) => new Fuel(fuel));
+    } catch (err) {
+      console.error("Error loading restaurants!", err);
+    }
+  },
+  states: async (req) => {
+    try {
+      const locations = await db.select().table("locations");
+      const states = {};
+      locations.forEach((location) => {
+        if (states[location.state] === undefined) {
+          states[location.state] = {
+            name: location.state,
+            cities: [],
+            highways: [],
+          };
+        }
+
+        if (!states[location.state].cities.includes(location.city)) {
+          states[location.state].cities.push(location.city);
+        }
+        if (!states[location.state].highways.includes(location.highway)) {
+          states[location.state].highways.push(location.highway);
+        }
+      });
+
+      return Object.values(states);
+    } catch (err) {
+      console.error("Error loading states!", err);
+    }
+  },
+};
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
+    schema,
     rootValue: root,
     graphiql: true,
   })
